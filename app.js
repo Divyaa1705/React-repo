9a385f9f006e5356a84d90354ecf336481044472
divyaa1705 <divyatripathi1705@gmail.com>
2024-02-13 16:39:11 +0530

    updatingstyle.css

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const parah = React.createElement("p" , {} , "i am divya tripathi vvvvvvvvvvvvvvvvv");
-const root = ReactDOM.createRoot(document.getElementById("Ration"));
-root.render(parah);
+const parah = React.createElement("p" , {id:"tulip"} , "i am divya tripathi vvvvvvvvvvvvvvvvv");
+const roof = ReactDOM.createRoot(document.getElementById("Ration"));
+roof.render(parah);
+
+const divya = React.createElement("div" , {id:"pare"} , [React.createElement("div" , {id:"chi"} , React.createElement("h1" , {} , "Amsterdam is a beautiful place"))]);
+const root = ReactDOM.createRoot(document.getElementById("paper"));
+root.render(divya);
